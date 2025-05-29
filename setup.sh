affc625a3b9002c5de5c21850f79ccb9d6af2aa4
Parikshit75 <pa751999@gmial.com>
2025-05-29 17:45:09 +0530

    setup is updated

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 #Intinalize spack and load reframe 
 source /home/apps/SPACK/spack/share/spack/setup-env.sh
-spack load reframe
-
+#spack load reframe
+spack load reframe@4.7.3
 #Enabling auto-completion for refarme 
 source /home/apps/SPACK/spack/opt/spack/linux-almalinux8-cascadelake/gcc-13.2.0/reframe-4.0.4-dwsezsgnraoadbyfzrdzygco5ghqeexv/share/completions/reframe.bash 
 
