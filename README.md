dc2b505abf955b3a69f04463731902b9491e2cf6
Parikshit75 <pa751999@gmail.com>
2026-03-15 13:23:56 +0530

    feat: Migrate Stream benchmarks, add HPCG and HPL microbenchmarks, and refactor the configuration system to a modular structure.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # HPC_Test_Suite
 HPC_Test_Suite is complete testing and benchmarking framework of HPC Applications.
  
+ reframe -S valid_systems=paramrudra.snbose:gpu -S valid_prog_environs=gnu -l
--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 # This files include all the intional requirement of the framwork
 
 #Intinalize spack and load reframe 
-source /home/apps/SPACK/spack/share/spack/setup-env.sh
+source /home/apps/spack/share/spack/setup-env.sh
 #spack load reframe
 spack load reframe@4.7.3
 #Enabling auto-completion for refarme 
-source /home/apps/SPACK/spack/opt/spack/linux-almalinux8-cascadelake/gcc-13.2.0/reframe-4.0.4-dwsezsgnraoadbyfzrdzygco5ghqeexv/share/completions/reframe.bash 
+source /home/apps/spack/opt/spack/linux-almalinux8-cascadelake/gcc-13.2.0/reframe-4.7.3-cr5giu3pqks37s6dazqrnfq2q7s55rga/share/completions/reframe.bash 
 
 #Set the paths for Input filse, Test Files, System Setting Files
-export RFM_CONFIG_PATH=$(pwd)/config/IUAC_spack #:$(pwd)/config/IUAC_module
+export RFM_CONFIG_PATH=$(pwd)/config/modular_config
+export RFM_OUTPUT_DIR=/scratch/$USER/output
+export RFM_STAGE_DIR=/scratch/$USER/stage
+export RFM_PERFLOG_DIR=/scratch/$USER/perflogs
+export RFM_CHECK_SEARCH_PATH=$(pwd)
+export RFM_CHECK_SEARCH_RECURSIVE=yes
+
